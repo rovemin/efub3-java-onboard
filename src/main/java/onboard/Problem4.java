7820665dbcb380205422c81ca996e1b2a7170b00
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-03-09 01:04:31 +0900

    ✨ feat: add Problem4

---
+++ src/main/java/onboard/Problem4.java
@@ -0,0 +1,12 @@
+package onboard;
+
+/**
+ * 리턴값 answer의 경우, 변수 초기값은 변경 가능합니다.
+ * 다만 answer로 return하는 것은 변경하지 않도록 주의해주세요.
+ */
+public class Problem4 {
+    public static Integer solution(Integer number){
+        Integer answer = 0;
+        return answer;
+    }
+}
