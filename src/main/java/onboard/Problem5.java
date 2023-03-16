35d6e3af987e63c1534b564d49722b61b8cf0e70
Minseo Cho <min1130@ewhain.net>
2023-03-16 23:48:31 +0900

    ✨ feat: solve Problem5

--- src/main/java/onboard/Problem5.java
+++ src/main/java/onboard/Problem5.java
@@ -3,6 +3,14 @@ package onboard;
 public class Problem5 {
     public static Integer solution(Integer N, Integer K, Integer[] values){
         Integer answer = 0;
+
+        for (int i=N-1; i>0; i--) {
+            if ((K / values[i]) > 0) {
+                answer += (K / values[i]);
+                K = K - ((K / values[i]) * values[i]);
+            }
+        }
+
         return answer;
     }
 }
