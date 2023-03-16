7f726ac2808e36013d180b90f93c94f42b6ac0e6
Minseo Cho <min1130@ewhain.net>
2023-03-16 23:57:05 +0900

    🎨 style: Problem5 주석 수정

--- src/main/java/onboard/Problem5.java
+++ src/main/java/onboard/Problem5.java
@@ -6,7 +6,8 @@ public class Problem5 {
 
         for (int i=N-1; i>0; i--) {
             if ((K / values[i]) > 0) {
-                answer += (K / values[i]);
+                answer += (K / values[i]);  // K원을 만드는 데 values[i] 종류의 동전이 몇 개 필요한지 구하고
+                                            // 지금까지 구한 필요한 동전 개수에 더하기
                 K = K - ((K / values[i]) * values[i]);
             }
         }
