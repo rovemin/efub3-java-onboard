ba9d0f2ee515dad98baf7fc348e994aca3f2eeb1
Minseo Cho <min1130@ewhain.net>
2023-03-16 23:31:45 +0900

    ✨ feat: solve Problem3

--- src/main/java/onboard/Problem3.java
+++ src/main/java/onboard/Problem3.java
@@ -1,11 +1,26 @@
 package onboard;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Problem3 {
     public static List<Integer> solution(List<Integer> numbers){
         List<Integer> answer = new ArrayList<>();
+
+        int sum = 0;
+
+        for (int num : numbers) {
+            sum += num;
+        }
+
+        answer.add(sum/5);  // 평균
+
+        Collections.sort(numbers);
+        numbers.toString();
+
+        answer.add(numbers.get(2)); // 중앙값
+
         return answer;
     }
 }
