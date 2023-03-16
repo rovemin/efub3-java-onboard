cfc1d3a7375815d78250af607e8563d8cf734cb8
Minseo Cho <min1130@ewhain.net>
2023-03-16 23:48:19 +0900

    ✨ feat: solve Problem2

--- src/main/java/onboard/Problem2.java
+++ src/main/java/onboard/Problem2.java
@@ -5,7 +5,24 @@ import java.util.List;
 
 public class Problem2 {
     public static Integer[] solution(Integer[][] flipCards){
-        Integer[] answer = {};
+        Integer[] answer = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+        for (int i=0; i<20; i++) {
+            answer[i] = i;
+        }
+
+        for (int i=0; i<flipCards.length; i++) {
+            Integer[] arr = flipCards[i];
+
+            int a = arr[0];
+            int b = arr[1];
+
+            for (int j=a; j<b-a+1; j++) {
+                for (int k=b; k>=j; k--)
+                answer[j-1] = answer[k-1];
+            }
+        }
+
         return answer;
     }
 }
